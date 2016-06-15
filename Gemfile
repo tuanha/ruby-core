6a20441559afa0856117289fb8e2d30563dda288
tuanha <tuanha@hiworld.com.vn>
2016-06-15 11:23:37 +0700

    User & Permission - Role management

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'devise-bootstrap-views'
 gem 'paperclip'
 gem 'kaminari'
 gem 'enumerize'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,15 @@ class AdminController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout 'admin'
+
+  before_action :authenticate_user!
+  before_action :check_admin
+
+  private
+
+  def check_admin
+    redirect_to '/', :flash => {danger: "You aren't as admin"} unless current_user.role.admin?
+  end
 end
