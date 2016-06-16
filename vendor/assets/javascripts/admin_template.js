85e7d6c763115b52bfeb1726c521a9be672a513f
Hà Hải Quân <quanhh@hiworld.com.vn>
2016-06-16 10:03:48 +0700

    Add theme smartadmin to project

---
+++ vendor/assets/javascripts/admin_template.js
@@ -0,0 +1,9 @@
+//= require jquery
+//= require ./admin/libs/jquery-ui-1.10.3.min.js
+//= require jquery_ujs
+//= require turbolinks
+//= require ./admin/app.config
+//= require twitter/bootstrap
+//= require ./admin/notification/SmartNotification.min
+//= require ./admin/smartwidgets/jarvis.widget.min.js
+//= require ./admin/app.min
