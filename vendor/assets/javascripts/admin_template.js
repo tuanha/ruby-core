afbb0841f09272a148d196845103ae0d8ae57135
Hà Hải Quân <quanhh@hiworld.com.vn>
2016-06-16 10:30:02 +0700

    Add js & css admin template

--- vendor/assets/javascripts/admin_template.js
+++ vendor/assets/javascripts/admin_template.js
@@ -1,3 +1,4 @@
+// SmartAdmin
 //= require jquery
 //= require ./admin/libs/jquery-ui-1.10.3.min.js
 //= require jquery_ujs
@@ -7,3 +8,4 @@
 //= require ./admin/notification/SmartNotification.min
 //= require ./admin/smartwidgets/jarvis.widget.min.js
 //= require ./admin/app.min
+
